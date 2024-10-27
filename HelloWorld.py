1f6e2868f2e14a48ce58b93834e1c65bb5eeebf1
diatsilva007 <diatsilva007@gmail.com>
2024-10-26 22:15:16 -0300

    feat: dependecies created and updated

---
+++ HelloWorld.py
@@ -0,0 +1,3 @@
+# Aulta teste
+
+print ("Olá Sou Diogo 👋🏻, sou engenheiro de software. Já sei programar e consigo rodar meus próprios scripts. ")
---
+++ debbuging/debbuging.py
@@ -0,0 +1,12 @@
+# Debbuging this code 
+
+print ("Por que o Python foi ao médico? Porque tinha problemas de 'indentação'!")
+print ("Quando a vida te der 'None', lembre-se: sempre há uma maneira de lidar com isso!")
+print ("Sinto que meu código está em um loop infinito... tipo uma série que nunca acaba.")
+print ("As exceções são como super-heróis: às vezes, você precisa chamá-las para salvar o dia!")
+print ("O que o programador disse ao seu computador? 'Debugue-me se for capaz!'")
+print ("Na batalha entre a variável e o 'garbage collector', quem leva a melhor?")
+print ("Se meu código fosse uma história, hoje seria um thriller de suspense: 'Onde estão meus bugs?'")
+print ("Sempre que vejo um erro de sintaxe, meu coração faz 'raise Exception'.")
+print ("Debugging: onde os sonhos de um código perfeito vão para morrer.")
+print ("Por que o código nunca se sente sozinho? Porque sempre tem um 'try' e um 'except'!")
