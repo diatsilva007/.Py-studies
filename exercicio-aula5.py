faea0960686404a1379920016495107f02c6863c
diatsilva007 <diatsilva007@gmail.com>
2024-10-27 22:45:45 -0300

    feat: lógica para resolver exercício da aula 5

---
+++ exercicio-aula5.py
@@ -0,0 +1,12 @@
+# EXERC: crie um programa que peça ao usuário que insira o seu nome, seu telefone e seu número de identidade.
+        # ao final imprima as informações inforamdas pelo usuário
+
+# A função 'input' recebe um texto como entrada e retorna esse texto como uma string.
+
+nome = input("Digite o seu nome: ")
+
+telefone = input("Digite seu telefone: ")
+
+identidade = input("Digite seu número de identidade: ")
+
+print("Seu nome é: ", nome + " Seu telefone é: " + telefone + " Sua identidade é: " + identidade)
