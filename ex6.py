0f4c82a998080a1d27d0dcad82b2114090473643
diatsilva007 <diatsilva007@gmail.com>
2024-10-27 23:03:12 -0300

    feat: implementada lógica para mostrar os dados solicitados pelo programa na tela

---
+++ ex6.py
@@ -0,0 +1,11 @@
+idade = 0
+altura = 0.0
+nome = ""
+
+idade = int(input("Digite sua idade: "))
+altura = float(input("Digite sua altura: "))
+nome = input("Digite seu nome: ")
+
+print ("Sua idade é: ", idade)
+print ("Sua altura é: ", altura)
+print ("Seu nome é: ", nome)
